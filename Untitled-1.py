2839197f5d4cd65c1fe4fb7b229a3a492462f714
tylercoy <tcoy001@fiu.edu>
2024-05-15 16:32:26 -0400

    new functionality to take user input and add it

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,8 @@
-print("Tyler ")
+def add_two_nums():
+        num1 = float(input("Enter the first Number: "))
+        num2 = float(input("Enter the second Number:"))
+        sum_nums = num1 + num2
+        print(f"The sum of {num1} and {num2} is {sum_nums}")
+
+add_two_nums()
+    
